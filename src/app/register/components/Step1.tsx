3a7f39b77700d863a02eae48d4538c2265173ddb
Paulo Bahia <pauloipatinga22@gmail.com>
2023-08-27 16:12:00 -0300

    Fix: Loading CheckEmail

--- src/app/register/components/Step1.tsx
+++ src/app/register/components/Step1.tsx
@@ -21,11 +21,13 @@ const Step1: React.FC<Step1Props> = ({ onNextStep, setEmail }) => {
     })
 
     const handleNext: SubmitHandler<CheckEmailFormData> = ({ email }) => {
+        setLoading(true)
         let postData = {
             email: email
         }
         checkEmail(postData)
             .then(() => {
+                setLoading(false)
                 onNextStep()
                 setEmail(email)
             })
@@ -49,9 +51,10 @@ const Step1: React.FC<Step1Props> = ({ onNextStep, setEmail }) => {
             <p className="font-light text-sm text-primary-secundary">Digite seu e-mail abaixo para criar sua conta</p>
             <form onSubmit={handleSubmit(handleNext)} className="w-full gap-y-2 flex mt-6 flex-col justify-center items-center">
                 <input {...register('email')} name='email' placeholder="nome@exemplo.com" type="email" className="flex placeholder:text-primary-secundary placeholder:text-xs w-full p-2 bg-transparent border text-sm border-primary-light text-white rounded-md" />
+                {errors.email && <span className='text-danger-base w-full text-xs font-semibold'>{errors.email.message}</span>}
                 <button disabled={loading} type='submit' className="bg-white transition-colors items-center flex justify-center gap-x-2 ease-in-out w-full p-2 mt-1 border text-black font-medium text-sm rounded-md hover:bg-transparent hover:border hover:border-gray-300 hover:text-white disabled:bg-neutral-500 disabled:border-0 disabled:hover:text-black">
                     {loading && <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />}
-                    Entrar com E-mail
+                    Criar com E-mail
                 </button>
             </form>
             <div className="mt-6 gap-x-3 flex items-center justify-center w-full">
