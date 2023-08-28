8e68941482fafe197d60d6a289c32964063b155c
Paulo Bahia <pauloipatinga22@gmail.com>
2023-08-28 00:31:35 -0300

    Fix: Build Theme Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ToastProvider } from '@/context/NotifyContext'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from '../provider/theme-provider'
+import { ThemeProvider } from '@/Provider/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
