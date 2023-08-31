aad507e1e5d8bd18f9fa7d9942a7b058bb6c8e68
Paolo <pauloipatinga22@gmail.com>
2023-08-31 13:02:07 -0300

    Style: Alterado icone botão de ação

--- src/app/(main)/products/components/columns.tsx
+++ src/app/(main)/products/components/columns.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/app/components/ui/dropdown-menu"
 import { Button } from "@/app/components/ui/button"
 
-import { MoreHorizontal } from "lucide-react"
+import { Edit } from "lucide-react"
 
 export type Products = {
   id: string
@@ -59,21 +59,10 @@ export const columns: ColumnDef<Products>[] = [
 
       return (
         <div className="flex justify-center items-center">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Abrir Menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Ações</DropdownMenuLabel>
-              <DropdownMenuItem>Editar</DropdownMenuItem>
-              <DropdownMenuItem>Favoritar</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Deletar</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Abrir Menu</span>
+            <Edit className="h-4 w-4" />
+          </Button>
         </div>
       )
     },
