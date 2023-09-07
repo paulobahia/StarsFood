895034b78c27d374f41fd9cfaac89f620933dc0f
Paulo Bahia <pauloipatinga22@gmail.com>
2023-09-07 17:09:39 -0300

    Feat: Implementações pontuais
    Implementado formatCurrency no input de valor do produto e implementado UseEffect para ao ser adicionado 3 imagens o foco da imagem principal ir para a primeira imagem adicionada

--- src/app/(main)/products/add-products/components/ImageProducts.tsx
+++ src/app/(main)/products/add-products/components/ImageProducts.tsx
@@ -15,6 +15,15 @@ const ImageProducts = ({ productsImage, onRemove }: ImageProductsProps) => {
         setSelectedImage('')
     }
 
+    useEffect(() => {
+
+        if (productsImage.every(item => item.imagePath !== '')) {
+            setSelectedImage(productsImage[0].imagePath)
+        }
+
+    }, [productsImage])
+
+
     return (
         <div className="bg-backgrounds-secondary p-5 gap-x-2 rounded-xl flex h-full items-center justify-start">
             <div className="h-52 sm:h-full w-[70%] flex flex-col items-center justify-center">
@@ -41,7 +50,7 @@ const ImageProducts = ({ productsImage, onRemove }: ImageProductsProps) => {
                                 :
                                 <div onClick={() => setSelectedImage(imagePath)} className={`w-full h-16 sm:h-[185px] justify-center items-center flex ${selectedImage != imagePath && 'opacity-50 cursor-pointer'}`} >
                                     {selectedImage == imagePath &&
-                                        <div onClick={() => handleRemove(index)} className="absolute cursor-pointer top-2 right-2">
+                                        <div onClick={() => handleRemove(index)} className="absolute bg-black/50 rounded-sm p-0.5 cursor-pointer top-2 right-2">
                                             <X className="w-6 h-6 text-white" />
                                         </div>
                                     }
--- src/app/(main)/products/add-products/components/table-cell.tsx
+++ src/app/(main)/products/add-products/components/table-cell.tsx
@@ -13,13 +13,27 @@ const TableCell = ({ getValue, row, column, table }: any) => {
         table.options.meta?.onEdit(row.index, column.id, value)
     }
 
+    const formatCurrency = (value: string) => {
+        const floatValue = parseFloat(value.replace(/\D/g, '')) / 100;
+        const formattedValue = floatValue.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        });
+        return formattedValue;
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setValue(formatCurrency(value));
+    };
+
     if (tableMeta?.editedRows[row.id]) {
         return (
             <div className="py-1 flex justify-center items-center">
                 <input
                     className="flex placeholder:text-primary-secundary w-28 text-center placeholder:text-xs p-2 bg-transparent border text-sm border-primary-light text-white rounded-md"
                     value={value}
-                    onChange={e => setValue(e.target.value)}
+                    onChange={handleChange}
                     onBlur={onBlur}
                 />
             </div>
